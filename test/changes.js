32872cb5d9cf30878a635794a6e0a55d8a9dd36e
Michel <orphee@gmail.com>
2015-05-19 23:15:11 -0700

    Add changes().limit

--- test/changes.js
+++ test/changes.js
@@ -22,7 +22,7 @@ describe('changes.js', function(){
       r.connect(config.rethinkdb).bind({}).then(function(conn) {
         connections.rethinkdb = conn;
         // Use the rethinkdb connection to validate the tests
-        //mainConnection = conn;
+        mainConnection = conn;
         return r.connect(config.reqlite);
       }).then(function(conn) {
         connections.reqlite = conn;
@@ -638,6 +638,28 @@ describe('changes.js', function(){
       }).then(function() {
         return r.db(TEST_DB).table(TEST_TABLE).getAll(10, 11, 12).delete().run(mainConnection);
       }).then(function() {
+        done();
+      });
+
+      return r.db(TEST_DB).table(TEST_TABLE).insert({id: 10}).run(mainConnection);
+    }).then(function() {
+      return r.db(TEST_DB).table(TEST_TABLE).insert({id: 11}).run(mainConnection);
+    }).then(function() {
+      return r.db(TEST_DB).table(TEST_TABLE).insert({id: 12}).run(mainConnection);
+    }).catch(done);
+  });
+
+  it('changes - 39', function(done) {
+    var query = r.db(TEST_DB).table(TEST_TABLE).changes().limit(2);
+    query.run(mainConnection).then(function(feed) {
+      feed.next().then(function(result) {
+        assert.deepEqual(result, {new_val: {id: 10}, old_val: null});
+        return feed.next();
+      }).then(function(result) {
+        assert.deepEqual(result, {new_val: {id: 11}, old_val: null});
+        return feed.next();
+      }).error(function(error) {
+        assert(error.message, 'No more rows in the Feed.');
         done();
       });
 
