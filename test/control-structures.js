ee061ac63266e4d6e1003f9fd6b44cc5f963728b
Michel <orphee@gmail.com>
2015-05-21 23:34:25 -0700

    Use different table for control

--- test/control-structures.js
+++ test/control-structures.js
@@ -5,7 +5,7 @@ var assert = require('assert');
 
 var connections = {};
 var TEST_DB = 'reqlitetest';
-var TEST_TABLE = 'reqlitetestaggregation';
+var TEST_TABLE = 'reqlitetestcontrol';
 var MISSING_ID = 'nonExistingId';
 var MISSING_FIELD = 'nonExistingField';
 var MISSING_INDEX = 'nonExistingIndex';
