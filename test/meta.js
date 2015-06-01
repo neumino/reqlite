7414fcf9db6cc4689f378cdc0429d93b89b21d0c
Michel <orphee@gmail.com>
2015-05-31 23:11:51 -0700

    Add cluster_config table - Related to #12

---
+++ test/meta.js
@@ -0,0 +1,97 @@
+var config = require(__dirname+'/../config.js');
+
+var r = require('rethinkdb');
+//var r = require('rethinkdbdash')({pool: false});
+var assert = require('assert');
+
+var connections = {};
+var TEST_DB = 'reqlitetest';
+var TEST_TABLE = 'reqlitetestmeta';
+var MISSING_ID = 'nonExistingId';
+var MISSING_FIELD = 'nonExistingField';
+var MISSING_INDEX = 'nonExistingIndex';
+var COMPLEX_OBJECT = {
+  id: 1,
+  foo: 2,
+  buzz: {
+    hello: [10, 11, 12],
+    world: {
+      nested: 100,
+      field: 200
+    },
+    bonjour: [
+      {monde: 1000, ciao: 4000},
+      {monde: 2000, ciao: 5000},
+      {monde: 3000, ciao: 6000},
+    ]
+  },
+}
+
+var compare = require(__dirname+'/util.js').generateCompare(connections);
+
+describe('meta.js', function(){
+  before(function(done) {
+    setTimeout(function() { // Delay for nodemon to restart the server
+      r.connect(config.rethinkdb).bind({}).then(function(conn) {
+        connections.rethinkdb = conn;
+        return r.connect(config.reqlite);
+      }).then(function(conn) {
+        connections.reqlite = conn;
+        this.query = r.dbCreate(TEST_DB);
+        return this.query.run(connections.rethinkdb);
+      }).catch(function(e) { // ignore errors
+        console.log(e);
+      }).finally(function() {
+        return this.query.run(connections.reqlite);
+      }).catch(function() { // ignore errors
+      }).finally(function() {
+        this.query = r.db(TEST_DB).tableDrop(TEST_TABLE)
+        return this.query.run(connections.rethinkdb);
+      }).catch(function() { // ignore errors
+      }).finally(function() {
+        return this.query.run(connections.reqlite);
+      }).catch(function() { // ignore errors
+      }).finally(function() {
+        done();
+      });
+    }, 300)
+  });
+
+
+  //Test playground
+
+
+  it('cluster_config - 1', function(done) {
+    var query = r.db('rethinkdb').table('cluster_config');
+    compare(query, done);
+  });
+
+  it('cluster_config - 2', function(done) {
+    var query = r.db('rethinkdb').table('cluster_config').insert({})
+    compare(query, done, function(result) {
+      delete result.generated_keys;
+      return result;
+    });
+  });
+
+  it('cluster_config - 3', function(done) {
+    var query = r.db('rethinkdb').table('cluster_config').get('auth').update({auth_key: 'foo'})
+    compare(query, done);
+  });
+
+  it('cluster_config - 4', function(done) {
+    var query = r.db('rethinkdb').table('cluster_config').get('auth')
+    compare(query, done);
+  });
+
+  it('cluster_config - 5', function(done) {
+    var query = r.db('rethinkdb').table('cluster_config').get('auth').update({auth_key: ''})
+    compare(query, done);
+  });
+
+  it('cluster_config - 6', function(done) {
+    var query = r.db('rethinkdb').table('cluster_config').get('auth')
+    compare(query, done);
+  });
+});
+
