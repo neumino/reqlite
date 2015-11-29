ed476b1def7071fa193ea0d7dc2de0f9c3ed014a
Michel Tu <orphee@gmail.com>
2015-11-29 01:20:33 -0800

    Move utils and add browserify script

---
+++ browserify.js
@@ -0,0 +1,41 @@
+var browserify = require('browserify');
+var fs = require('fs');
+
+var REQUIRE_FILES = [
+	'asc.js',
+  'changes.js',
+  'connection.js',
+  'constants.js',
+  'database.js',
+  'date.js',
+  'desc.js',
+  'document.js',
+  'error.js',
+  'geo.js',
+  'group.js',
+  'index.js',
+  'literal.js',
+  'maxval.js',
+  'minval.js',
+  'missing_doc.js',
+  'node.js',
+  'protodef.js',
+  'query.js',
+  'selection.js',
+  'sequence.js',
+  'table.js',
+  'utils.js'
+];
+
+var b = browserify('./lib/')
+b.add('./lib/index.js')
+for(var i=0; i<REQUIRE_FILES.length; i++) {
+  b.require('./lib/'+REQUIRE_FILES[i], {expose: './lib/'+REQUIRE_FILES[i]})
+}
+b.bundle(function(err, result) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  fs.writeFileSync('./browser/index.js', result);
+});
