17d237c77b69d723c5b88572e0d83ebd5b4400c1
Michel Tu <orphee@gmail.com>
2015-11-30 21:16:10 -0800

    Rename test.js to example.js

---
+++ browser/example.js
@@ -0,0 +1,49 @@
+var Reqlite = require('reqlite');
+var r = require('rethinkdbdash')({pool: false});
+
+var server = new Reqlite();
+var fakeTcpConnection = server.createConnection();
+
+r.connect({
+  connection: fakeTcpConnection
+}).bind({}).then(function(connection) {
+  this.connection = connection;
+  console.log('Connected');
+  return r.expr('Ok').run(this.connection);
+}).then(function(result) {
+  console.log(JSON.stringify(result, null, 2));
+  return r.dbCreate('blog').run(this.connection);
+}).then(function(result) {
+  console.log(JSON.stringify(result, null, 2));
+  return r.db('blog').tableCreate('post').run(this.connection);
+}).then(function(result) {
+  console.log(JSON.stringify(result, null, 2));
+  return r.db('blog').table('post').insert([{
+    title: 'First post',
+    author: 'Michel'
+  },
+  {
+    title: 'Second post',
+    author: 'Daniel'
+  },
+  ]).run(this.connection);
+}).then(function(result) {
+  console.log(JSON.stringify(result, null, 2));
+  return r.db('blog').table('post').run(this.connection);
+}).then(function(result) {
+  console.log(JSON.stringify(result, null, 2));
+  return r.db('blog').table('post').filter({title: 'First post'}).update({
+    updated: r.now()
+  }).run(this.connection);
+}).then(function(result) {
+  console.log(JSON.stringify(result, null, 2));
+  return r.db('blog').table('post').run(this.connection);
+}).then(function(result) {
+  console.log(JSON.stringify(result, null, 2));
+  return r.http('http://httpbin.org').run(this.connection);
+}).then(function(result) {
+  console.log(JSON.stringify(result, null, 2));
+  console.log('Done');
+}).catch(function(error) {
+  console.log(error);
+});
